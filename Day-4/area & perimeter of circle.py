e308c1a4880ab43e967076919cc0329d09be8d61
Aayush <ay4503301@gmail.com>
2026-01-06 18:38:22 +0530

    Add files via upload

---
+++ Day-4/area & perimeter of circle.py
@@ -0,0 +1,6 @@
+import math
+radius=float(input("enter the radius of the circle:"))
+area=math.pi*radius*radius
+perimeter=2*math.pi*radius
+print("area of circle=",area)
+print("perimeter of circle=",perimeter)
